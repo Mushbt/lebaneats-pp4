c43732b5c1a8616b2c3f84c333b96de87a39f2e8
Mushbt <mustafa_habet@hotmail.com>
2023-10-01 12:23:47 +0000

    Add commenting functionality

--- lebaneats/settings.py
+++ lebaneats/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blog',
 ]
 
@@ -44,6 +46,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
