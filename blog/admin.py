788ee1497f1e3fd9726779c3c0b28efcf26f71d7
Mushbt <mustafa_habet@hotmail.com>
2023-10-01 15:08:28 +0000

    Add function to admin.py file to allow registered user to add recipes

--- blog/admin.py
+++ blog/admin.py
@@ -9,7 +9,12 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
+    blog_fields = ('content')
     summernote_fields = ('content')
+    actions = ['approve_recipes']
+
+    def approve_recipes(self, request, queryset):
+        queryset = update(status=1)
 
 
 @admin.register(Comment)
