61a4feff2f21a9746c3a824c83675fbc30fae713
Mushbt <mustafa_habet@hotmail.com>
2023-10-03 07:46:08 +0000

    Add delete recipe to urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('share_recipe/', views.add_recipe, name="add_recipe"),
     path('edit_recipe/<slug:slug>', views.edit_recipe, name="edit_recipe"),
+    path('delete_recipe/<slug:slug>', views.delete_recipe, name="delete_recipe"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
