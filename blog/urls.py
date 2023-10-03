ad54ffe31973ed70685f96b8eb518889b2929d48
Mushbt <mustafa_habet@hotmail.com>
2023-10-03 07:26:09 +0000

    Add edit and add recipes to urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('about/', views.about, name='about'),
+    path('share_recipe/', views.add_recipe, name="add_recipe"),
+    path('edit_recipe/<slug:slug>', views.edit_recipe, name="edit_recipe"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
