f9aad8460904e1a20e4c78b3bd07838e39d10787
Mushbt <mustafa_habet@hotmail.com>
2023-10-04 15:09:25 +0000

    Reset debug and disable_collectstatic to continue styling and cleaning up site

--- lebaneats/settings.py
+++ lebaneats/settings.py
@@ -17,7 +17,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-mushbt-lebaneatspp4-ik88vbxrqw8.ws-eu105.gitpod.io', 'lebaneats.herokuapp.com', 'localhost', 'lebaneats-e428f9dc7d63.herokuapp.com']
 
